ed42c7090a1ef5567b33aaba1d6c31ec56fded25
A.R.ANEESH <142220243008.ai@srmveccampus.onmicrosoft.com>
2023-09-24 17:12:37 +0530

    updated everythin

--- components/navbar/Sidebar.js
+++ components/navbar/Sidebar.js
@@ -102,7 +102,7 @@ function Sidebar({ isMounted, unmount }) {
           </a>
         </Link>
         <a
-          href="https://docs.google.com/forms/d/1eOZsO1X6qmqyS48nEw43RJnOhwEBuAvXpXZXMtOOLaY/viewform?edit_requested=true"
+          href="/forms"
           className="flex rounded-full"
           rel="noreferrer"
         >
